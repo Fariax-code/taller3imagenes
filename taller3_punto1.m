21fab278e4d6db25a34ef870cf0b7c572278c505
Fariax-code <est.jhoseph.farias@unimilitar.edu.co>
2021-03-16 22:30:52 -0500

    4

--- taller3_punto1.m
+++ taller3_punto1.m
@@ -33,34 +33,34 @@ imshow(filtro);
 
 function v=calfiltro(x)
 
-
-v=x+150;
+% 
+% v=x+150;
 
 % z=unit8(b);
-% if x ~= 0
-% 
-% [fila,colum] = find(x);
-% 
-%   m(1,1)= x(fila-1+z,colum-1+z);
-%   m(1,2)= x(fila-1+z,colum+z);
-%   m(1,3)= x(fila-1+z,colum+1+z);
-%   
-%   m(2,1)= x(fila+z,colum-1+z);
-%   m(2,2)= x(fila+z,colum+z);
-%   m(2,3)= x(fila+z,colum+1+z);
-%   
-%   m(3,1)= x(fila+1+z,colum-1+z);
-%   m(3,2)= x(fila+1+z,colum+z);
-%   m(3,3)= x(fila+1+z,colum+1+z);
-%   
-%   multi=m.*mascara;
-%   
-%   suma=sum(multi,'all');
-%  
-%   v=suma;
-% else
-%     v=x;
-% end
+if x ~= 0
+
+[fila,colum] = find(x);
+
+  m(1,1)= x(fila-1+z,colum-1+z);
+  m(1,2)= x(fila-1+z,colum+z);
+  m(1,3)= x(fila-1+z,colum+1+z);
+  
+  m(2,1)= x(fila+z,colum-1+z);
+  m(2,2)= x(fila+z,colum+z);
+  m(2,3)= x(fila+z,colum+1+z);
+  
+  m(3,1)= x(fila+1+z,colum-1+z);
+  m(3,2)= x(fila+1+z,colum+z);
+  m(3,3)= x(fila+1+z,colum+1+z);
+  
+  multi=m.*mascara;
+  
+  suma=sum(multi,'all');
+ 
+  v=suma;
+else
+    v=x;
+end
 
 % if IGr ~= 0
 %   [z,y] = find(IGr);
